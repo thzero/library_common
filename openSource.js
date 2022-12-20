a968f98b25332e958a7b9b2b1f9e54d3c58c2533
thzero <thzero@users.noreply.github.com>
2022-12-20 08:00:55 -0600

    updates for opensource

---
+++ openSource.js
@@ -0,0 +1,46 @@
+export default () => {
+    return [
+        {
+            category: 'server',
+            name: '@thzero/library_common',
+            url: 'https://github.com/thzero/library_common',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_common/blob/master/license.md'
+        },
+        {
+            category: 'server',
+            name: 'dayjs',
+            url: 'https://github.com/iamkun/dayjs',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/iamkun/dayjs/blob/dev/LICENSE'
+        },
+        {
+            category: 'server',
+            name: 'dayjs-plugin-utc',
+            url: 'https://github.com/guisturdy/dayjs-plugin-utc',
+            licenseName: '??',
+            licenseUrl: ''
+        },
+        {
+            category: 'client',
+            name: 'lodash',
+            url: 'https://github.com/lodash/lodash',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/lodash/lodash/blob/master/LICENSE'
+        },
+        {
+            category: 'server',
+            name: 'short-uuid',
+            url: 'https://github.com/oculus42/short-uuid',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/oculus42/short-uuid/blob/develop/LICENSE'
+        },
+        {
+            category: 'server',
+            name: 'uuid',
+            url: 'https://github.com/kelektiv/node-uuid',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/kelektiv/node-uuid/blob/master/LICENSE.md'
+        }
+    ];
+}
